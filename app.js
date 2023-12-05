20972164aa32c0de4c313890fe716df48bb48fa1
Vítor Cunha <142564603+VitorCunhaDePaula@users.noreply.github.com>
2023-12-05 11:40:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 (function (){
 
-let button = document.querySelectorAll("input");
-let result = document.querySelector(".resultP");
+let button = document.querySelectorAll("input");   //Elemento do dom onde estão os botões da calculadora.
+let result = document.querySelector(".resultP");   //Elemento do dom referente ao visor da calculadora.
 
 button.forEach(btn =>{
-    btn.addEventListener("click", function(){
-        if(btn.value === "="){
-        result.textContent = eval(result.textContent)
+    btn.addEventListener("click", function(){           // Evento criado após algum botão ser clicado.
+        if(btn.value === "="){                          // ------> Quando o usuário apertar o botão "=" a função eval() será chamada passando todos os valores que estão                             
+        result.textContent = eval(result.textContent)   // -----> no visor, retornando ao visor o resultado.
         }
-        else if(btn.value === "c"){
+        else if(btn.value === "c"){        // ---> Quando o usuário apertar no botão "C" todos os valores do visor serão deletados.
         result.textContent = "";
         }
         
-        else if (btn.value === "+") {
+        else if (btn.value === "+") {             
            
-        if (!result.textContent.includes("+")) {
+        if (!result.textContent.includes("+")) {    // Impede que o usuário adicione dois "+" um ao lado do outro.
                 result.textContent += btn.value;
         }}
 
         else if (btn.value === "-") {
            
-        if (!result.textContent.includes("-")) {
+        if (!result.textContent.includes("-")) {      // Impede que o usuário adicione dois "-" um ao lado do outro.
                     result.textContent += btn.value;
                 }}
 
-        else if (btn.value === "/") {
+        else if (btn.value === "/") {               // Impede que o usuário adicione dois "/" um ao lado do outro.
            
         if (!result.textContent.includes("/")) {
                         result.textContent += btn.value;
@@ -32,29 +32,29 @@ button.forEach(btn =>{
 
         else if (btn.value === "*") {
            
-        if (!result.textContent.includes("*")) {
+        if (!result.textContent.includes("*")) {               // Impede que o usuário adicione dois "*" um ao lado do outro.
                             result.textContent += btn.value;
                         }}
 
-        else if (btn.value === "%") {
+        else if (btn.value === "%") {               
            
-        if (!result.textContent.includes("%")) {
+        if (!result.textContent.includes("%")) {          // Impede que o usuário adicione dois "%" um ao lado do outro.
         result.textContent += btn.value;
         }}
 
 
         else if (btn.value === ".") {
            
-            if (!result.textContent.includes(".")) {
+            if (!result.textContent.includes(".")) {      // Impede que o usuário adicione dois "." um ao lado do outro.
                 result.textContent += btn.value;
             }}
 
-         else if(btn.value === "x"){
+         else if(btn.value === "x"){                      // Quando o usuário clicar no "X", apenas o último valor do visor será removido.
                 result.textContent= result.textContent.toString().slice(0, -1);
                 }
         
         else{
-            result.textContent += btn.value;
+            result.textContent += btn.value;     // Adiciona os botões apertados no visor.
         }
 
     })
